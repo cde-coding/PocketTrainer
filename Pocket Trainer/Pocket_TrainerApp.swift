e843f903dd4cca211dee1049acd990d073955dfe
Christian Espinoza <improvementchimp@gmail.com>
2023-02-17 12:02:45 -0800

    Initial Commit

---
+++ Pocket Trainer/Pocket_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_TrainerApp.swift
+//  Pocket Trainer
+//
+//  Created by Diego Espinoza on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
